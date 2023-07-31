051726a3e9838dd8d1896eb176c31bbe43d6bb42
LuSot00 <LS@gmail.com>
2023-07-31 15:27:39 +0300

    Add Exercise 54

---
+++ Exercise54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matr = new int[n, m];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ArrangeNumbers(int[,] arr)
+{
+    int[] array = new int[arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            array[j] = arr[i, j];
+        }
+
+        Array.Sort(array);
+        Array.Reverse(array);
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = array[j];
+        }
+    }
+    return arr;
+}
+
+int[,] result = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(result);
+Console.WriteLine();
+Console.WriteLine("Упорядоченный по строчкам массив: ");
+PrintMatrix(ArrangeNumbers(result));
