eb3e3d1da7030e7244f9572ca267216105792648
LuSot00 <LS@gmail.com>
2023-07-31 15:46:46 +0300

    Add Exercise 56

---
+++ Exercise56/Program.cs
@@ -0,0 +1,72 @@
+﻿
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+if (rows == columns)
+{
+    Console.WriteLine("Массив не прямоугольный!");
+    return;
+}
+
+int[,] GetMatrix(int n, int m, int min, int max)
+{
+    int[,] matr = new int[n, m];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindRow(int[,] arr)
+{
+    int minIndexRow = 0;
+    int min = int.MaxValue;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum = sum + arr[i, j];
+        }
+        if (sum < min)
+        {
+            min = sum;
+            minIndexRow = i;
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой: {minIndexRow + 1}");
+}
+
+int[,] array2D = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+FindRow(array2D);
